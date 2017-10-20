245d9df8a3858c2f7529b51a05c941dbb031a8ba
harry <harmanmanchanda182@gmail.com>
2017-10-21 01:47:59 +0530

    Hello Vue!

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1> Hello, {{ formatName(user) }}! </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      user: {
+        firstName: 'Harry',
+        lastName: 'Manchanda'
+      }
+    };
+  },
+  methods: {
+    formatName(user) {
+      return `${user.firstName} ${user.lastName}`;
+    }
+  }
+}
+</script>
